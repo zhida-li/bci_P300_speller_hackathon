55cddf822b88e90ec7933e6e18a762a5018b078d
Ramy ElMallah <ramyelmallah@gmail.com>
2022-04-30 16:40:49 -0700

    Added code and dataset for BCI

--- VFBLS_VCFBLS_demo.py
+++ VFBLS_VCFBLS_demo.py
@@ -22,6 +22,16 @@ import os
 import sys
 import time
 import random
+import scipy.io
+import matplotlib.pyplot as plt 
+import numpy as np
+import scipy.stats
+import random
+from scipy import signal
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
+
 
 # Import external libraries
 import numpy as np
@@ -34,6 +44,7 @@ from bls.model.vfbls_train_fscore import vfbls_train_fscore
 from bls.model.vcfbls_train_fscore import vcfbls_train_fscore
 from bls.processing.feature_select_cnl import feature_select_cnl
 
+from sklearn.model_selection import train_test_split
 
 # import warnings
 # warnings.filterwarnings("ignore", category=FutureWarning)
@@ -52,24 +63,58 @@ def enablePrint():
 
 # ============================================== may edit --begin
 # Load the datasets
-train_dataset = np.loadtxt("./slammer_64_train.csv", delimiter=",")
-test_dataset = np.loadtxt("./slammer_64_test.csv", delimiter=",")
+mats = []
+for i in range(1, 6):
+    mats.append(scipy.io.loadmat(f'S{i}.mat'))
 
+SAMPLING_FREQUENCY=250
+PRE_SAMPLES = int(-0.1*SAMPLING_FREQUENCY) # -0.2
+POST_SAMPLES = int(0.6*SAMPLING_FREQUENCY) # 0.4
+
+p_samples = {}
+n_samples = {}
+
+for s in range(len(mats)):
+    p_samples[s] = []
+    n_samples[s] = []
+    for i in range(len(mats[s]['trig'])):
+        if mats[s]['trig'][i] == 0:
+            pass #ignore this
+        elif mats[s]['trig'][i] == 1:
+            p_samples[s].append((i-PRE_SAMPLES, i+POST_SAMPLES))
+        elif mats[s]['trig'][i] == -1:
+            n_samples[s].append((i-PRE_SAMPLES, i+POST_SAMPLES))
+
+# let's consider one subject for now, and then we can concat all together after if we want
+# consider subject 0 for now
+subject_index = 0
+x = mats[subject_index]['y']
+
+y = np.zeros(len(mats[subject_index]['trig']))
+for start_i, end_i in p_samples[subject_index]:
+    y[start_i:end_i] = 1
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, shuffle=False)
+# print(train_x.shape, test_x.shape, train_y.shape, test_y.shape)
+print(np.sum(train_y == 1), np.sum(test_y == 1))
+# train_dataset = np.loadtxt("./slammer_64_train.csv", delimiter=",")
+# test_dataset = np.loadtxt("./slammer_64_test.csv", delimiter=",")
+# exit(0)
 # Normalize training data
-train_x = train_dataset[:, 0:-1]
+# train_x = train_dataset[:, 0:-1]
 train_x = zscore(train_x, axis=0, ddof=1)  # For each feature, mean = 0 and std = 1
 replaceNan(train_x)  # Replace "nan" with 0
-train_y = train_dataset[:, -1]
+# train_y = train_dataset[:, -1]
 
 # Change training labels
 inds1 = np.where(train_y == 0)
 train_y[inds1] = 2
 
 # Normalize test data
-test_x = test_dataset[:, 0:-1]
+# test_x = test_dataset[:, 0:-1]
 test_x = zscore(test_x, axis=0, ddof=1)  # For each feature, mean = 0 and std = 1
 replaceNan(test_x)  # Replace "nan" with 0
-test_y = test_dataset[:, -1]
+# test_y = test_dataset[:, -1]
 
 # Change test labels
 inds1 = np.where(test_y == 0)
@@ -97,7 +142,7 @@ N2_bls_fsm1 = 10  # feature groups in the 3rd set
 N1_bls_fsm2 = 20  # feature nodes of a group in the 3rd set
 N2_bls_fsm2 = 10  # feature groups in the 3rd set
 
-N3_bls_fsm = 200  # enhancement nodes
+N3_bls_fsm = 100  # enhancement nodes
 
 add_nFeature1 = 6  # no. of top relevant features in the 2nd set
 add_nFeature2 = 4  # no. of top relevant features in the 3rd set
